07f0fa25210fe99f06b814fdb591256d9fdb5f6c
Sasha Arrabi <arraba1@nationwide.com>
2019-07-18 23:24:15 -0400

    Add detection of money fixing the house

--- src/main.js
+++ src/main.js
@@ -18,6 +18,8 @@ window.loadGame = () => {
   Q.gravityY = 2000;
 
   Q.boxes = 0;
+  Q.score = 0;
+  Q.houses = [];
 
   Q.include("Audio").enableSound();
 
--- src/sprite/money.js
+++ src/sprite/money.js
@@ -16,7 +16,17 @@ export const Money = {
     this.p.angle += 10;
     this.p.scale *= .985;
 
-    if (this.p.y < 350) { this.p.scale *= .9}
+    if (this.p.y < 350) {
+      this.p.scale *= .9
+
+      for (let i = 0; i < Q.houses.length; i++) {
+        if (this.p.x >= Q.houses[i].x && this.p.x < Q.houses[i].x + 100) {
+          Q.houses[i].fix();
+          Q.score++;
+        }
+      }
+    }
+
     if (this.p.y < 300) { 
       this.destroy(); 
       Q.boxes--;
