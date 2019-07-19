7483406de59f95b5b85749e146d742584b791e3a
Shilling, Brian <SHILLB3@nationwide.com>
2019-07-18 23:11:29 -0400

    asdfasdfasdfasdf

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import { Box } from './sprite/box';
 import { Driver } from './sprite/driver';
 import { Player } from './sprite/player';
 import { Wheel } from './sprite/wheel';
+import { House } from './sprite/house';
 
 window.addEventListener("load", function () {
 
- var Q = window.Q = Quintus({audioSupported: ['mp3', 'ogg']})
+  var Q = window.Q = Quintus({ audioSupported: ['mp3', 'ogg'] })
     .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
     .setup({ maximize: true })
     .controls().touch();
@@ -26,25 +27,39 @@ window.addEventListener("load", function () {
   Q.Sprite.extend("Box", Box);
 
   Q.Sprite.extend("Wheel", Wheel);
-
+  
   Q.GameObject.extend("BoxThrower", BoxThrower);
+
+  Q.Sprite.extend("House", House);
 
   Q.scene("level1", Level1);
 
-  Q.load("driver.png, player.png, wheel-source.png, sky.png, road.png, crates.png, crates.json, grass.png, vehicle.png, money.png, jingle.mp3", function () {
-    Q.compileSheets("crates.png", "crates.json");
-    Q.stageScene("level1");
+  Q.load(["driver.png",
+    'player.png',
+    'wheel-source.png',
+    'sky.png',
+    'road.png',
+    'crates.png',
+    'crates.json',
+    'grass.png',
+    'vehicle.png',
+    'money.png',
+    'house1.png',
+    'house2.png',
+    'jingle.mp3'], function () {
+      Q.compileSheets("crates.png", "crates.json");
+      Q.stageScene("level1");
 
-    function playThemeSong () {
-      Q.audio.stop('jingle.mp3');
-      Q.audio.play('jingle.mp3', {
-        loop: true,
-        loopStart: 0,
-        loopEnd: 51
-      });
-    }
-    playThemeSong();
-  });
+      function playThemeSong() {
+        Q.audio.stop('jingle.mp3');
+        Q.audio.play('jingle.mp3', {
+          loop: true,
+          loopStart: 0,
+          loopEnd: 51
+        });
+      }
+      playThemeSong();
+    });
 });
 
 
