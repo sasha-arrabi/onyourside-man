b81595d74fc45a7823730aeff85c6f9edce464d0
Sasha Arrabi <arraba1@nationwide.com>
2019-07-18 20:11:54 -0400

    More webpack fixes. I'm not stupid I swear.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
 const _root = path.resolve(__dirname, '.');
 function root(args) {
